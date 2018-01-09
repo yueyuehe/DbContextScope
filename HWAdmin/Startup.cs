1111111afe4f8f0ea4f30e9c41a96e13e6276b2b
hewei <13558705505@163.com>
2018-01-09 16:04:43 +0800

    新增layUI

---
+++ HWAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HWAdmin.Startup))]
+namespace HWAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
